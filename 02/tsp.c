dc173457cb9fd9159b2f1a506a7de2739865b53e
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-05-08 22:38:27 -0300

    nit

--- 02/tsp.c
+++ 02/tsp.c
@@ -3,28 +3,39 @@
 #include <math.h>
 #include <time.h>
 
-// Struct para representar o ponto
-typedef struct Ponto {
-	int x, y;
-} Ponto;
+// Calcula a distância euclidiana entre p1 e p2
+double dist(unsigned int p1, unsigned int p2) {
+	return sqrt( (p1 - p2)*(p1 - p2) +
+				(p1 - p2)*(p1 - p2)
+			);
+}
 
-// Cria uma lista pontos P baseado no arquivo passado como parâmetro
-void createPointsList(FILE *file, Ponto* pontos) {
-    Ponto p;
+// Cria uma matriz de adjacências baseado no arquivo passado como parâmetro
+void criaGrafo(FILE *file, int n_pontos, unsigned int grafo[n_pontos][n_pontos]) {
+    unsigned int x, y;
 
 	int ret = 0;
-	int n_lines = 0;
+	int i, j = 0;
 	char line_buffer[100]; //buffer a ser utilizado na leitura de cada linha
 
 	// Percorre a stream, identifica as duas coordenadas na linha
-	// e insere o valor no array pontos
+	// e insere o valor na matriz de ajacências
 	while(fgets(line_buffer, sizeof(line_buffer), file)) {
-		ret = sscanf(line_buffer, "%d %d", &p.x, &p.y);
-		if(ret < 1)
+		ret = sscanf(line_buffer, "%d %d", &x, &y);
+        
+        if(ret < 1)
 			continue;
-		pontos[n_lines].x = p.x;
-		pontos[n_lines].y = p.y;
-		n_lines++;
+
+        if(i == j) {
+            grafo[i][j] = 0;
+            i++, j++;
+            continue;
+        }
+
+        printf("%d %d\n", x, y);
+
+		grafo[i][j] = dist(x, y);
+		i++, j++;
 	}
 }
 
@@ -33,14 +44,14 @@ int main(int argc, char *argv[]) {
 
     /* Checa argumentos da linha de comando */
     if (argc != 2) {
-        printf ("ERROR: you must specify file name!\n");
+        printf ("ERRO: especifique o nome do arquivo!\n");
         return 1;
     }
 
     /* Abre o arquivo */
     file = fopen(argv[1], "r");
     if (!file) {
-        perror ("File open error!\n");
+        perror ("Erro ao abrir o arquivo!\n");
         return 1;
     }
 	
@@ -49,9 +60,13 @@ int main(int argc, char *argv[]) {
     fscanf(file, "%s", first_line); // Le a primeira linha do arquivo para saber quantos pares foram gerados
     sscanf(first_line, "%d", &n_pontos);//converte string para int
 
-    Ponto pontos[n_pontos];
+    unsigned int grafo[n_pontos][n_pontos];
 
-	createPointsList(file, pontos);// Cria lista de pares com base no arquivo input.txt
+	criaGrafo(file, n_pontos, grafo);// Cria grafo com base no arquivo input.txt
+
+    // for (int i = 0; i < n_pontos; ++i)
+	// 	for (int j = i+1; j < n_pontos; ++j)
+    //         printf("custo %d %d: %d\n", i, j, grafo[i][j]);
 
     fclose (file);
 }
