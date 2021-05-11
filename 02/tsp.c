980135f26cb089192334aa2151ec2c2c8d4c8dc7
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-05-11 20:22:42 -0300

    init matrix

--- 02/tsp.c
+++ 02/tsp.c
@@ -3,39 +3,43 @@
 #include <math.h>
 #include <time.h>
 
+// Struct para representar o ponto
+typedef struct Ponto {
+	unsigned int x, y;
+    float distancia;
+} Ponto;
+
 // Calcula a distância euclidiana entre p1 e p2
-double dist(unsigned int p1, unsigned int p2) {
-	return sqrt( (p1 - p2)*(p1 - p2) +
-				(p1 - p2)*(p1 - p2)
-			);
+float dist(Ponto p1, Ponto p2) {
+	return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
 // Cria uma matriz de adjacências baseado no arquivo passado como parâmetro
-void criaGrafo(FILE *file, int n_pontos, unsigned int grafo[n_pontos][n_pontos]) {
-    unsigned int x, y;
+void criaGrafo(FILE *file, int n_pontos, Ponto grafo[n_pontos][n_pontos]) {
+    Ponto p;
 
 	int ret = 0;
-	int i, j = 0;
+	int i = 0;
 	char line_buffer[100]; //buffer a ser utilizado na leitura de cada linha
 
 	// Percorre a stream, identifica as duas coordenadas na linha
 	// e insere o valor na matriz de ajacências
 	while(fgets(line_buffer, sizeof(line_buffer), file)) {
-		ret = sscanf(line_buffer, "%d %d", &x, &y);
+		ret = sscanf(line_buffer, "%d %d", &p.x, &p.y);
         
         if(ret < 1)
 			continue;
 
-        if(i == j) {
-            grafo[i][j] = 0;
-            i++, j++;
-            continue;
-        }
+        //Inicializa coluna 0 e linhas y = i
+		grafo[i][0].x = p.x;
+        grafo[i][0].y = p.y;
+        grafo[i][0].distancia = -1;
+        //Inicializa linha 0 e colunas y = i
+        grafo[0][i].x = p.x;
+        grafo[0][i].y = p.y;
+        grafo[0][i].distancia = -1;
 
-        printf("%d %d\n", x, y);
-
-		grafo[i][j] = dist(x, y);
-		i++, j++;
+		i++;
 	}
 }
 
@@ -60,13 +64,12 @@ int main(int argc, char *argv[]) {
     fscanf(file, "%s", first_line); // Le a primeira linha do arquivo para saber quantos pares foram gerados
     sscanf(first_line, "%d", &n_pontos);//converte string para int
 
-    unsigned int grafo[n_pontos][n_pontos];
+    Ponto grafo[n_pontos][n_pontos];
 
 	criaGrafo(file, n_pontos, grafo);// Cria grafo com base no arquivo input.txt
 
-    // for (int i = 0; i < n_pontos; ++i)
-	// 	for (int j = i+1; j < n_pontos; ++j)
-    //         printf("custo %d %d: %d\n", i, j, grafo[i][j]);
+    for (int i = 0; i < n_pontos; ++i)
+        printf("%d %d\n", grafo[0][i].x, grafo[0][i].y);
 
     fclose (file);
 }
