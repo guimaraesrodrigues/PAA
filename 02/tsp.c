320e6e73675ee30fcd7502205414959e98961d98
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-05-12 04:44:30 -0300

    preencheGrafo function

--- 02/tsp.c
+++ 02/tsp.c
@@ -37,11 +37,11 @@ float dist(Ponto p1, Ponto p2) {
 	return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
-Aresta inicializaAresta(Ponto p1, Ponto p2) {
+Aresta inicializaAresta(Ponto inicio, Ponto fim) {
     Aresta a;
-    a.inicio = p1;
-    a.fim = p2;
-    a.distancia = dist(p1, p2);
+    a.inicio = inicio;
+    a.fim = fim;
+    a.distancia = dist(inicio, fim);
 
     return a;
 }
@@ -50,10 +50,10 @@ Aresta inicializaAresta(Ponto p1, Ponto p2) {
 //Ex. da matriz apos inicializacao, onde x eh lixo e [A..E] sao struct Aresta:
 /**
  * A B C D E
- * B x x x x
- * C x x x x
- * D x x x x
- * E x x x x
+ * B B x x x
+ * C x C x x
+ * D x x D x
+ * E x x x E
  **/
 void criaGrafo(FILE *file, int n_pontos, Aresta grafo[n_pontos][n_pontos]) {
     Ponto p;
@@ -70,22 +70,44 @@ void criaGrafo(FILE *file, int n_pontos, Aresta grafo[n_pontos][n_pontos]) {
         if(ret < 1)
 			continue;
 
-        //Inicializa coluna 0 e linhas x = i
-		grafo[i][0].inicio.x = p.x;
-        grafo[i][0].inicio.y = p.y;
-        //Inicializa linha 0 e colunas y = i
+        //Inicializa linha 0
         grafo[0][i].inicio.x = p.x;
         grafo[0][i].inicio.y = p.y;
 
-        if (i > 0 && i <= n_pontos) {
-            grafo[i][0] = inicializaAresta(grafo[0][0].inicio, grafo[i][0].inicio);
-            grafo[0][i].distancia =  grafo[i][0].distancia;
-            grafo[0][i].inicio = grafo[i][0].fim;
-            grafo[0][i].fim = grafo[i][0].inicio;
-        }
+        //Inicializa coluna 0 
+		grafo[i][0].inicio.x = p.x;
+        grafo[i][0].inicio.y = p.y;
+
+        grafo[i][0] = inicializaAresta(grafo[i][0].inicio, grafo[0][0].inicio);
+
+        grafo[0][i].distancia =  grafo[i][0].distancia;
+        grafo[0][i].inicio = grafo[i][0].fim;
+        grafo[0][i].fim = grafo[i][0].inicio;
+        
+        // Inicializa diagonal da matriz
+        grafo[i][i].inicio = grafo[i][0].inicio;
+        grafo[i][i].fim = grafo[i][0].inicio;
+        grafo[i][i].distancia = 0;
 
 		i++;
 	}
+}
+
+/**
+ * 
+ * 
+ **/
+void preencheGrafo(int n_pontos, Aresta grafo[n_pontos][n_pontos]) {
+    for(int i = 1; i < n_pontos; i++)
+        for(int j = 1; j < n_pontos; j++) 
+            if( i != j) {
+                grafo[i][j] = inicializaAresta(grafo[i][j-1].fim, grafo[i][j].inicio);
+                grafo[j][i].distancia =  grafo[i][j].distancia;
+                
+                grafo[j][i].inicio = grafo[i][j].fim;
+                grafo[j][i].fim = grafo[i][j].inicio;
+            } 
+
 }
 
 int main(int argc, char *argv[]) {
@@ -113,10 +135,15 @@ int main(int argc, char *argv[]) {
 
 	criaGrafo(file, n_pontos, grafo);// Cria grafo com base no arquivo input.txt
 
-   for(int i = 0; i < n_pontos; i ++) {
-       printf("(%f ,%f) -> (%f, %f)\n", grafo[i][0].inicio.x, grafo[i][0].inicio.y, grafo[i][0].fim.x, grafo[i][0].fim.y);
-       printf("dist: %f\n", grafo[i][0].distancia);
-   }
+    preencheGrafo(n_pontos, grafo);
+
+   for(int i = 0; i < n_pontos; i ++)
+    for(int j = 0; j < n_pontos; j ++) {
+        printf("%d %d\n", i, j);
+        printf("(%.0f ,%.0f)->(%.0f, %.0f)\n", grafo[i][j].inicio.x, grafo[i][j].inicio.y, grafo[i][j].fim.x, grafo[i][j].fim.y);
+        printf("--------------\n");
+        // printf("dist: %.2f\n", grafo[i][j].distancia);
+    }
 
     fclose (file);
 }
