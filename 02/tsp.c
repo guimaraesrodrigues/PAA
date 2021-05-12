7be22ae8fbacdef2006575405c847987cc25573c
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-05-12 11:49:20 -0300

    remove dummy code

--- 02/tsp.c
+++ 02/tsp.c
@@ -9,7 +9,6 @@
  * **/
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 #include <math.h>
 #include <time.h>
 #include <float.h>
@@ -115,88 +114,6 @@ void preencheGrafo(int n_pontos, Aresta grafo[n_pontos][n_pontos]) {
             } 
 
 }
-
-int ehLaco(Aresta a) {
-    if((a.inicio.x == a.fim.x) && (a.inicio.y == a.fim.y))
-        return 1;
-    
-    return 0;
-}
-
-// A utility function to find the vertex with 
-// minimum key value, from the set of vertices 
-// not yet included in MST 
-int minKey(float key[], int mstSet[], int n_vertices) 
-{ 
-    // Initialize min value 
-    float min = INT_MAX, min_index; 
-  
-    for (int v = 0; v < n_vertices; v++) 
-        if (mstSet[v] == 0 && key[v] < min) 
-            min = key[v], min_index = v; 
-  
-    return min_index; 
-} 
-
-// A utility function to print the 
-// constructed MST stored in parent[] 
-void printMST(int parent[], int n_vertices, Aresta graph[n_vertices][n_vertices]) 
-{ 
-    for (int i = 1; i < n_vertices; i++) 
-        printf("%d - %d\t%f\n", parent[i], i, graph[i][parent[i]].distancia); 
-} 
-
-// Function to construct and print MST for 
-// a graph represented using adjacency 
-// matrix representation 
-void primMST(int n_vertices, Aresta grafo[n_vertices][n_vertices]) 
-{ 
-    // Array to store constructed MST 
-    int parent[n_vertices]; 
-      
-    // Key values used to pick minimum weight edge in cut 
-    float key[n_vertices]; 
-      
-    // To represent set of vertices included in MST 
-    int mstSet[n_vertices]; 
-  
-    // Initialize all keys as INFINITE 
-    for (int i = 0; i < n_vertices; i++) 
-        key[i] = INT_MAX, mstSet[i] = 0; 
-  
-    // Always include first 1st vertex in MST. 
-    // Make key 0 so that this vertex is picked as first vertex. 
-    key[0] = 0; 
-    parent[0] = -1; // First node is always root of MST 
-  
-    // The MST will have V vertices 
-    for (int count = 0; count < n_vertices - 1; count++)
-    { 
-        // Pick the minimum key vertex from the 
-        // set of vertices not yet included in MST 
-        int u = minKey(key, mstSet, n_vertices); 
-  
-        // Add the picked vertex to the MST Set 
-        mstSet[u] = 1; 
-  
-        // Update key value and parent index of 
-        // the adjacent vertices of the picked vertex. 
-        // Consider only those vertices which are not 
-        // yet included in MST 
-        for (int v = 0; v < n_vertices; v++) 
-  
-            // graph[u][v] is non zero only for adjacent vertices of m 
-            // mstSet[v] is false for vertices not yet included in MST 
-            // Update the key only if graph[u][v] is smaller than key[v] 
-            if (!ehLaco(grafo[u][v]) && mstSet[v] == 0 && grafo[u][v].distancia < key[v]) 
-                parent[v] = u, key[v] = grafo[u][v].distancia; 
-    } 
-  
-    // print the constructed MST 
-    // printMST(parent, graph); 
-}
-
-
 
 void prim(int n_pontos, Aresta grafo[n_pontos][n_pontos], Ponto vertices[]) {
     // algoritmo guloso = escolhe algum pra começar e nao testa outros
