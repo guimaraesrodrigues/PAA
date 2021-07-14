40235d116c555ea9e20ee549d307e7b62a4d61af
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-07-14 17:44:20 -0300

    sort points based on x coord

--- 01/hull.c
+++ 01/hull.c
@@ -8,6 +8,80 @@ typedef struct Ponto {
 	float x, y;
 } Ponto;
 
+void merge(Ponto arr[], int esq, int mid, int dir, char eixo) {
+	int i, j, k;
+	int n1 = mid - esq + 1;
+	int n2 = dir - mid;
+
+	Ponto L[n1], R[n2];
+
+	/* Copia dados para arrays temporarios L[] e R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[esq + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[mid + 1+ j];
+
+	/* Faz merge dos arrays temporarios em arr[esq..dir]*/
+	i = 0; 
+	j = 0; 
+	k = esq;
+	while (i < n1 && j < n2) {
+		if (eixo == 'y') {
+			if (L[i].y <= R[j].y) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+		} else {
+			if (L[i].x <= R[j].x) {
+				arr[k] = L[i];
+				i++;
+			} else {
+				arr[k] = R[j];
+				j++;
+			}
+		}
+		k++;
+	}
+
+	/* Copia os elementos restanted de  L[], se existirem */
+	while (i < n1) {
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copia os elementos restanted de  R[], se existirem */
+	while (j < n2) {
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+void mergeSort(Ponto arr[], int esq, int dir, char eixo) {
+	if (esq < dir) {
+		int mid = esq+(dir-esq)/2;
+
+		// Ordena a primeira e a segunda metade
+		mergeSort(arr, esq, mid, eixo);
+		mergeSort(arr, mid+1, dir, eixo);
+
+		merge(arr, esq, mid, dir, eixo);
+	}
+}
+
+float calcDist(Ponto p1, Ponto p2, Ponto p3) {
+    float numerador = (
+            (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x) *
+            (p2.x - p1.x) * (p3.y - p1.y) - (p2.y - p1.y) * (p3.x - p1.x)
+        );
+    float denominador = ((p2.x - p1.x) * (p2.x - p1.x) + (p2.y - p1.y) * (p2.y - p1.y));
+    return sqrt(numerador)/sqrt(denominador);
+}
+
 // Cria uma lista pontos P baseado no arquivo passado como parâmetro
 void createPointsList(FILE *file, Ponto* pontos) {
     Ponto p;
@@ -26,6 +100,10 @@ void createPointsList(FILE *file, Ponto* pontos) {
 		pontos[n_lines].y = p.y;
 		n_lines++;
 	}
+}
+
+void quickHull(Ponto* pontos) {
+
 }
 
 int main(int argc, char *argv[]) {
@@ -52,8 +130,11 @@ int main(int argc, char *argv[]) {
     Ponto pontos[n_pontos];
 
 	createPointsList(file, pontos);// Cria lista de pares com base no arquivo input.txt
-
     fclose (file);
+
+    mergeSort(pontos, 0, n_pontos - 1, 'x');// Ordena lista com base na cordenada x de cada ponto
+
+    quickHull(pontos);
 
     for (int i = 0; i < n_pontos; i++) {
         printf("%f, %f\n", pontos[i].x, pontos[i].y);
