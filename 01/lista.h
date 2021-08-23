c50b23e3dbad6d5aa213f2b2b0a5e3fcd30090d0
Rodrigo Guimarães <guimaraes_rodrigues@yahoo.com>
2021-08-23 14:54:01 -0300

    remove list files

---
+++ 01/lista.h
@@ -0,0 +1,36 @@
+#ifndef LISTA_H
+#define LISTA_H
+
+//////////////////// STRUCTS ////////////////////
+// Prototipos de struct para tipagem dos prototipos de função de uma lista encadeada;
+
+// Struct para representar um nó de uma lista
+typedef struct Lista Lista;
+
+// Struct para representar uma lista
+typedef struct No No;
+
+// Struct para representar o ponto
+typedef struct Ponto Ponto;
+
+//////////////////// FUNÇÕES ////////////////////
+
+// Função para criar uma struct lista com os valores inicializados
+Lista* criar_lista();
+
+// Função para inserir uma struct Ponto de uma lista, em um índice específico
+void inserir(Lista* lista, Ponto p, int indice);
+
+// Função para inserir uma struct Ponto em uma lista na primeira posição
+void inserir_primeiro(Lista* lista, Ponto p);
+
+// Função para remover uma struct Ponto de uma lista
+Ponto remover(Lista* lista, Ponto* p);
+
+// Função para apagar uma lista
+void apagar_lista(Lista* lista);
+
+// Função para imprimir lista
+void imprimir_lista(Lista* lista);
+
+#endif // LISTA_H
