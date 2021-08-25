83cbab9aef69789dc74890a19cc8d4bee405cafd
felipemani <felipem@alunos.utfpr.edu.br>
2021-08-25 09:51:23 -0300

    comentarios explosivos

--- 02/utils/structs.h
+++ 02/utils/structs.h
@@ -1,7 +1,16 @@
 #ifndef STRUCTS_H
 #define STRUCTS_H
 
-// Struct para representar o ponto
+// Estrutura Ponto:
+// armazena um ponto do Grafo
+// Utilizado para:
+// - armazenar elementos do vector fecho convexo
+// - armazenar elementos do vector ciclo
+// - armazenar elementos do vector pontos_internos
+// Utilização:
+// - função removePontos();
+// - função gravaCiclo();
+// - função tsp();
 typedef struct Ponto {
 	float x, y;
 } Ponto;
